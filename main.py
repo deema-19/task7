28f1658a70f5d10dec12004d7c594bcc77fb9f88
deema-19 <deemajenana12@gmail.com>
2023-08-10 22:37:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# q1:
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(j+1, end=" ")
+#     print()
+
+
+# q2:
+# numbers = int(input("enter your numbers:"))
+# sum=0
+# for i in range(1 , numbers+1):
+#   sum+=i
+# print(sum)
+
+
+# q3:
+# name=input("enter your name:")
+# for i in range(0, len(name)):
+#     print(name[:i+1])
+# for i in range(len(name)):
+#     print(name[:len(name)-i])
+
+
+# q4:
+# word = input("Enter a word: ")
+# word_2= word[::-1]
+# print("Reversed word:", word_2)
+
+
+# q5:
+# x = int(input("Enter the range: "))
+# while x >= 1:
+#     print(x, end=" ")
+#     x -= 1
+
+
+# q6:
+# num = 5
+# count = 1
+# while count <= 10:
+#     multiple = num * count
+#     print(multiple, end=" ")
+#     count += 1
+
+# q7:
+# Limit_number = int(input("Enter the Limit_number: "))
+# Max_display_on_screen = int(input("Enter the maximum outputs to display (Max_display_on_screen): "))
+# Target_number = int(input("Enter the Target_number: "))
+# count = 0
+# multiple = Target_number
+# while multiple <= Limit_number and count < Max_display_on_screen:
+#     print(multiple, end=" ")
+#     multiple += Target_number
+#     count += 1
